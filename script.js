6557e4c837e288b1cb20e3c0aef9dc566d5d0c3b
admin505 <100965441+admin505admin505@users.noreply.github.com>
2023-12-12 09:50:56 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+    const audio = document.getElementById('music');
+audio.addEventListener('ended', function() {
+  // play next song
+  // you'd need to implement logic for playing the next song here
+});
